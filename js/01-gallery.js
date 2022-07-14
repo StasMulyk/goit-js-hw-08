374fae07bdb6daa3865fd9156c24d45db286eb7d
StasMulyk <stasmulyk@gmail.com>
2022-07-14 20:26:45 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
 
 console.log(galleryItems);
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player'); 
+const player = new Player(iframe);
+
+player.on('timeupdate',throttle(timeTracking,1000));
+
+function timeTracking (evt) {
+let currentTime = evt.seconds;
+localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));   
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
